25d1eddaac3304edd97cb3b282a3a0bb1c8fdffd
infinityglobusfl-maker <infinityglobus.fl@gmail.com>
2026-04-24 05:29:17 -0700

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def health():
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8080))
     uvicorn.run(app, host="0.0.0.0", port=port)
 
 @app.get("/")
